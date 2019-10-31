ab62ca3af515f9e1f64dc9c1f6a387b1ec3484d8
45501751+s650179@users.noreply.github.com <45501751+s650179@users.noreply.github.com>
2019-10-31 13:46:33 -0500

    BankAccount Class Finished

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,43 @@
 public class BankAccount {
+    private String name;
+    private String accountNumber;
+    private double savings;
+    private static int nextAccountNumber = 1;
+
+    public BankAccount (String name, String accountNumber, double savings) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.savings = savings;
+    }
+
+    public BankAccount (String name, double savings) {
+        this.name = name;
+        this.accountNumber = String.format("%6d", nextAccountNumber);
+        this.savings = savings;
+        nextAccountNumber += 1;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setSavings(double savings) {
+        this.savings = savings;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public double getSavings() {
+        return this.savings;
+    }
 }
---
+++ src/MainBank.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class MainBank {
+    private JTextArea textArea1;
+}
